0f5f4250e3952b4d11cb38649f7f6711d33eb9c3
AshenTrickstar <93138182+AshenTrickstar@users.noreply.github.com>
2023-08-23 16:52:08 -0300

    Add files via upload

---
+++ ac1 bhaskara.py
@@ -0,0 +1,16 @@
+A = float(input("Me informe o valor de A:"))
+B = float(input("Me informe o valor de B:"))
+C = float(input("Me informe o valor de C:"))
+
+Delta = (B ** 2) - (4 * A * C)
+
+float(Delta)
+
+equacao = (-B + (Delta ** 0.5)) / (2 * A)
+equacao2 = (-B - (Delta ** 0.5)) / (2 * A)
+
+print(equacao)
+print(equacao2)
+
+
+
